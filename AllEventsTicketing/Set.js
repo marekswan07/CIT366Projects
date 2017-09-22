c0f3fedb5c000bfc6eb42919c15dc938f5f9369c
marekswan07 <marekswan07@hotmail.com>
2017-09-21 21:21:00 -0600

    Working version

---
+++ AllEventsTicketing/Set.js
@@ -0,0 +1,89 @@
+function Set() {
+	
+	
+	this.intersection = function(listA, listB) {
+    
+	   var resultList = []; //create a result list array
+
+			if (listA === null || listB === null ){ // checks for null inputs
+				return null; // exit and return null error
+			}
+
+			for (var i  = 0; i < listA.length; i++ ){
+				var nextValue = listA[i];
+
+				for (var j = 0; j < listB.length; j++){
+					if(listB[j] === nextValue){
+						resultList.push(listB[j]);
+						break;
+					}
+				}
+			}
+       
+	   return resultList;
+	}
+    
+    
+    
+	this.union = function(listA, listB) {
+
+	   var resultList = [];
+
+	   var SD = this.symmetricDifference(listA,listB);
+	   var inter = this.intersection(listA,listB);
+
+	   resultList = SD.concat(inter);
+
+	   resultList.push();
+
+
+	   return resultList;
+	}
+
+
+
+
+	this.relativeComplement = function(listA, listB) {
+
+	   var resultList = [];
+
+        if (listA === null || listB === null ){ // checks for null inputs
+            return null; // exit and return null error
+        }
+
+        for(var i = 0; i < listA.length; i++){ //loop every element in listA
+        	for(var j = 0; j < listB.length;j++){ // loop every element in listB for each element in A
+        		var found = false; //this sets to true if a matching element is found
+        		if (listA[i] == listB[j]){ //if listA is equal to List B, break the loop and start on new element
+        			found = true;
+        			break;
+				}
+            }
+            if (found != true){
+        		resultList.push(listA[i]); // after every element in listB, if found is still false, add to result array
+			}
+		}
+
+	   return resultList;
+	}
+
+
+
+	this.symmetricDifference = function(listA, listB) {
+
+	   var resultList = [];
+
+	   var RCA = this.relativeComplement(listA,listB); //gets RC for A/B
+	   var RCB = this.relativeComplement(listB,listA); //gets RC for B/A
+
+	   resultList = RCA.concat(RCB); // joins together RCA and RCB arrays into resultList array as elements.
+
+	   resultList.push(); // push results to
+
+       
+	   return resultList;
+
+	}
+	
+
+}
